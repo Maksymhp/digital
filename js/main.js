5d2ff5ca4d937ecd706e7833eb239b1ceb453f06
Maksymhp <111457593+Maksymhp@users.noreply.github.com>
2023-07-06 19:40:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -69,7 +69,6 @@ var swiper = new Swiper(".mySwiper", {
     
   });
 
-
   AOS.init({
     // Global settings:
     disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
@@ -92,3 +91,73 @@ var swiper = new Swiper(".mySwiper", {
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
   
   });
+
+  var modal = document.getElementById ('myModal');
+  var modal2 = document.getElementById ('myModal2');
+  var modal3 = document.getElementById ('myModal3');
+  var modal4 = document.getElementById ('myModal4');
+  var btn = document.getElementById ("myBtn");
+  var btn2 = document.getElementById ("myBtn2");
+  var btn3 = document.getElementById ("myBtn3");
+  var btn4 = document.getElementById ("myBtn4");
+  var span = document.getElementsByClassName("close")[0];
+  var span2 = document.getElementsByClassName("close2")[0];
+  var span3 = document.getElementsByClassName("close3")[0];
+  var span4 = document.getElementsByClassName("close4")[0];
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }  
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  btn2.onclick = function() {
+    modal2.style.display = "block";
+  }
+
+  span2.onclick = function() {
+    modal2.style.display = "none";
+  }
+
+  btn3.onclick = function() {
+    modal3.style.display = "block";
+  }
+
+  span3.onclick = function() {
+    modal3.style.display = "none";
+  }
+
+  btn4.onclick = function() {
+    modal4.style.display = "block";
+  }
+
+  span4.onclick = function() {
+    modal4.style.display = "none";
+  }
+
+ 
+
+  window.onclick = function(event) {
+    if (Event.target == modal) {
+    modal.style.display = "none";
+  }}
+
+  window.onclick = function(event) {
+    if (Event.target == modal2) {
+    modal2.style.display = "none";
+  }}
+
+  window.onclick = function(event) {
+    if (Event.target == modal3) {
+    modal3.style.display = "none";
+  }}
+
+  window.onclick = function(event) {
+    if (Event.target == modal4) {
+    modal4.style.display = "none";
+  }
+
+ 
+  }
+  
